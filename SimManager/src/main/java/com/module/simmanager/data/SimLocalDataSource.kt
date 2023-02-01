d90b15de4cea1ede59db14273bdff02d8a046d6a
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-02-02 08:52:23 +0900

    feat: SimLocalDataSource 인테페이스 추가

---
+++ SimManager/src/main/java/com/module/simmanager/data/SimLocalDataSource.kt
@@ -0,0 +1,19 @@
+package com.module.simmanager.data
+
+import com.module.simmanager.model.SimItem
+
+class SimLocalDataSource {
+
+    /**
+     * 디바이스 모든 USIM, eSIM 리스트를 반환한다.
+     * @return ArrayList<SimItem>?
+     */
+    fun getAllSimList(): ArrayList<SimItem?>? {
+        try {
+
+        } catch (e: Exception) {
+
+        }
+        return null
+    }
+}
